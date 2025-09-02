ab68c1f59cbbf834f72a106ac31548b1852630f0
Roshan Singh <rs602543@gmail.com>
2025-09-02 17:11:32 +0000

    its still now working here are the rules ; rules_version = '2';
    service

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,25 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
+import { getFirestore, Firestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "apiKey",
-  authDomain: "authDomain",
-  projectId: "projectId",
-  storageBucket: "storageBucket",
-  messagingSenderId: "messagingSenderId",
-  appId: "appId"
+  apiKey: "AIzaSyDPp_T2a4n-52a-rMvGk71-yS0gDqJ-33Y",
+  authDomain: "dev-prototyper-231221.firebaseapp.com",
+  projectId: "dev-prototyper-231221",
+  storageBucket: "dev-prototyper-231221.appspot.com",
+  messagingSenderId: "329188812676",
+  appId: "1:329188812676:web:65f7c35f6a15757d5942ea"
 };
 
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
+// Initialize Firebase for SSR
+let app: FirebaseApp;
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+const db: Firestore = getFirestore(app);
 
 export { app, db };
